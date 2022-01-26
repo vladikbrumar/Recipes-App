6c5cb50b3552918cbbc15d116c41f029737d1cb6
Vladyslav <vladikbraun@gmail.com>
2022-01-25 16:15:23 -0800

    Initial Commit

---
+++ Recipes App/Recipes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipes_AppApp.swift
+//  Recipes App
+//
+//  Created by Vladyslav Brumar on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
